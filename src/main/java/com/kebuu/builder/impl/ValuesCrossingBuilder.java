03ae38a9d4cc6dda862a30dc316c8567a929b3ae
christophe tardella <christophe.tardella@dict.fr>
2014-08-29 16:29:51 +0200

    Code inspection

--- src/main/java/com/kebuu/builder/impl/ValuesCrossingBuilder.java
+++ src/main/java/com/kebuu/builder/impl/ValuesCrossingBuilder.java
@@ -51,9 +51,8 @@ public class ValuesCrossingBuilder extends AbstractBuilder {
         this(attribute1.getName(), createValueTransformer(attribute1), constantValue);
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    private static <T> Function<BuiltCotation, Optional<Double>> createValueTransformer(CotationAttribute attribute) {
-        return (BuiltCotation x) -> x.getValueByAttribute(attribute);
+    private static Function<BuiltCotation, Optional<Double>> createValueTransformer(CotationAttribute<Double> attribute) {
+        return (BuiltCotation x) -> x.getValueByAttribute(attribute).get().getValue();
     }
 
     @Override
