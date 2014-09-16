8e6ccb13f5a3334a40ff78f66ea081c4bbe1d506
christophe tardella <christophe.tardella@dict.fr>
2014-09-16 14:11:21 +0200

    Ajout TU

--- src/test/java/com/kebuu/flyway/FlywayTests.java
+++ src/test/java/com/kebuu/flyway/FlywayTests.java
@@ -2,13 +2,16 @@ package com.kebuu.flyway;
 
 import com.kebuu.Application;
 import org.flywaydb.core.Flyway;
-import org.flywaydb.core.internal.dbsupport.JdbcTemplate;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={Application.class})
 public class FlywayTests {
 
