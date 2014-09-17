167d72bac579a2e20d62ecb74552c5343644e35f
christophe tardella <christophe.tardella@dict.fr>
2014-09-17 13:58:27 +0200

    Ajout TODO + Correction ValueComparisonPosition

--- src/main/java/com/kebuu/enums/ValueComparisonPosition.java
+++ src/main/java/com/kebuu/enums/ValueComparisonPosition.java
@@ -10,9 +10,9 @@ public enum ValueComparisonPosition {
     public static ValueComparisonPosition from(double value1, double value2, double equalUnderThisAbsoluteValue) {
         double difference = value2 - value1;
 
-        if (difference <= -equalUnderThisAbsoluteValue) {
+        if (difference <= -equalUnderThisAbsoluteValue && difference != 0) {
             return FIRST_VALUE_LOWER;
-        } else if (difference >= equalUnderThisAbsoluteValue) {
+        } else if (difference >= equalUnderThisAbsoluteValue && difference != 0) {
             return FIRST_VALUE_UPPER;
         } else {
             return EQUAL_VALUES;
