ebf596b91bbb18b5c34a7c9b81252796f84fe631
Christophe TARDELLA <christophe.tardella@gmail.com>
2014-11-05 07:29:15 +0100

    Filtre par code

--- src/main/java/com/kebuu/web/controller/ExportDataController.java
+++ src/main/java/com/kebuu/web/controller/ExportDataController.java
@@ -9,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cotations")
+@RequestMapping("/cotations/{code}")
 @Slf4j
 public class ExportDataController {
 
@@ -23,26 +24,30 @@ public class ExportDataController {
     @Autowired private CotationRepository cotationRepository;
 
     @RequestMapping(value = "/csv", method = RequestMethod.GET)
-    public String getBuiltCotationsAsCsv() {
-        Cotations cotations = new Cotations(cotationRepository.findAll());
+    public String getBuiltCotationsAsCsv(@PathVariable("code") String code) {
+        Cotations cotations = getCotations(code);
         return exportDataService.toCsv(compositeCotationBuilder.attributes(), compositeCotationBuilder.basedOn(cotations));
     }
 
     @RequestMapping(value = "/csv/file", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public Resource getBuiltCotationsAsCsvFile() {
-        String fileContent = getBuiltCotationsAsCsv();
+    public Resource getBuiltCotationsAsCsvFile(@PathVariable("code") String code) {
+        String fileContent = getBuiltCotationsAsCsv(code);
         return new ByteArrayResource(fileContent.getBytes());
     }
 
     @RequestMapping(value = "/arff", method = RequestMethod.GET)
-    public String getBuiltCotationsAsArff() {
-        Cotations cotations = new Cotations(cotationRepository.findAll());
+    public String getBuiltCotationsAsArff(@PathVariable("code") String code) {
+        Cotations cotations = getCotations(code);
         return exportDataService.ToArff(compositeCotationBuilder.attributes(), compositeCotationBuilder.basedOn(cotations));
     }
 
     @RequestMapping(value = "/arff/file", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public Resource getBuiltCotationsAsArffFile() {
-        String fileContent = getBuiltCotationsAsArff();
+    public Resource getBuiltCotationsAsArffFile(@PathVariable("code") String code) {
+        String fileContent = getBuiltCotationsAsArff(code);
         return new ByteArrayResource(fileContent.getBytes());
+    }
+
+    private Cotations getCotations(String code) {
+        return new Cotations(cotationRepository.findByCode(code));
     }
 }
